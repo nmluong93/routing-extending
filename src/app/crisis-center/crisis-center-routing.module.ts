89cb13fad07dd195914ba7220c9c43224b5ce5ed
luongnm <luong.nguyen@adnovum.vn>
2021-03-28 11:43:57 +0700

    add Resolver and CanDeactivate guard

--- src/app/crisis-center/crisis-center-routing.module.ts
+++ src/app/crisis-center/crisis-center-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CanDeactivateGuard } from '../can-deactivate.guard';
 import { CrisisCenterHomeComponent } from './crisis-center-home/crisis-center-home.component';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { CrisisDetailResolverService } from './crisis-detail-resolver.service';
 import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
@@ -14,7 +16,13 @@ const crisisCenterRoutes: Routes = [
     children: [
       { path: '', component: CrisisListComponent,
         children: [
-          { path: ':id', component: CrisisDetailComponent
+          { 
+            path: ':id', 
+            component: CrisisDetailComponent,
+            canDeactivate : [CanDeactivateGuard],
+            resolve: {
+              crisis : CrisisDetailResolverService // define the crisis property of CrisisDetailComponent which handled by the CrisisDetailResolverService
+            }
           },
           { path: '', component: CrisisCenterHomeComponent
           }
--- src/app/crisis-center/crisis-detail/crisis-detail.component.ts
+++ src/app/crisis-center/crisis-detail/crisis-detail.component.ts
@@ -4,6 +4,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Crisis } from '../crisis';
 import { CrisisService } from '../crisis.service';
+import { DialogService } from 'src/app/dialog.service';
  
 
 @Component({
@@ -13,12 +14,14 @@ import { CrisisService } from '../crisis.service';
 })
 export class CrisisDetailComponent implements OnInit {
 
-  crisis$!: Observable<Crisis | undefined>;
+  crisis!: Crisis;
+  editName! : string;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private service: CrisisService
+    private service: CrisisService,
+    public dialogService: DialogService
   ) { }
 
   ngOnInit() {
@@ -30,21 +33,43 @@ export class CrisisDetailComponent implements OnInit {
     /**
      * Use the observable paramMap approach if there's a possibility that the router could re-use the component. 
      * This tutorial sample app uses with the observable paramMap.
+     * 
+        this.crisis$ = this.route.paramMap.pipe(
+          switchMap((params: ParamMap) => this.service.getCrisis(params.get('id')!))
+        );
      */
-    this.crisis$ = this.route.paramMap.pipe(
-      switchMap((params: ParamMap) => this.service.getCrisis(params.get('id')!)
-      )
-    );
+    
+    this.route.data
+      .subscribe((data: { crisis?: Crisis }) => {
+        this.editName = data.crisis!.name;
+        this.crisis = data.crisis!;
+      });
   }
 
-  gotoCrisises(crisis: Crisis) {
+  gotoCrises(crisis?: Crisis) {
     const crisisId = crisis ? crisis.id : null;
     // Relative navigation back to the crises
     this.router.navigate(['../', { id: crisisId, foo: 'foo' }], { relativeTo: this.route });
   }
+
+  save() {
+    this.crisis.name = this.editName;
+    this.gotoCrises();
+  }
+  cancel() {
+    this.gotoCrises();
+  }
+
+  // canDeactivate(): Observable<boolean> | boolean {
+  //   // Allow synchronous navigation (`true`) if no crisis or the crisis is unchanged
+  //   if (!this.crisis || this.crisis.name === this.editName) {
+  //     return true;
+  //   }
+  //   // Otherwise ask the user with the dialog service and return its
+  //   // observable which resolves to true or false when the user decides
+  //   return this.dialogService.confirm('Discard changes?');
+  // }
 }
-
-
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
